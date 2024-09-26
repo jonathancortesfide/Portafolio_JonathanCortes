516fac0d0cd19068bb414dfc915fc77122f6393d
jonathancortesfide <jcortes20382@ufide.ac.cr>
2024-09-25 20:05:25 -0600

    Creación de Proyecto
    Se creó proyecto desde 0

---
+++ TiendaJonathanCortes/src/main/java/Tienda/demo/TiendaJonathanCortesApplication.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJonathanCortesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJonathanCortesApplication.class, args);
+	}
+
+}
